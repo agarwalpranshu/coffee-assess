cfe66cf574f945d21756638ac6497e0894f39eeb
Pranshu Agarwal <pranshu@ambertech.in>
2022-09-17 00:24:35 +0530

    project initially created

---
+++ src/main/java/com/elane/coffee/CoffeeAssessApplication.java
@@ -0,0 +1,13 @@
+package com.elane.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAssessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeAssessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elane/coffee/CoffeeAssessApplicationTests.java
@@ -0,0 +1,11 @@
+package com.elane.coffee;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAssessApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
